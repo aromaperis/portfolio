fb7628f56858a2743e3f5a6179d4fbcf8fd98e34
Aroma3008 <paroma3008@gmail.com>
2024-01-15 17:59:57 +0530

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), tailwind({
+    applyBaseStyles: false
+  }), icon({
+      include: {
+        mdi: ["*"]
+      }
+  })]
+});
